eb252e248db9510c8cecec111865ef8145c776e1
Thomas P. <91093536+plagakit@users.noreply.github.com>
2025-01-24 17:13:30 -0500

    Add class brief

--- Engine/src/audio/audio.h
+++ Engine/src/audio/audio.h
@@ -2,6 +2,11 @@
 
 #include "core/resource/resource.h"
 
+/**
+A resource that stores an Audio (on Windows it
+stores the path to the sound and plays it using
+SimpleSound w/o exposing <App> to any users.)
+*/
 class Audio : public Resource
 {
 public:
